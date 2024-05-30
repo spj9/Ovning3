311195f92d1688f64dbe1d8fd55ba72e6e06a89d
spj9 <sanny_j@hotmail.com>
2024-05-30 13:01:34 +0200

    Person class

---
+++ Ovning3/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+//Person-klassen
+public class Person
+{
+    private int _age;
+    private string _fName;
+    private string _lName;
+    private int _height;
+    private int _weight;
+
+    public Person()
+	{
+        FName = fName;
+        LName = lName;
+    }
+
+    //Age
+    public int Age
+    {
+        get => _age;
+        set
+        {
+            if (value > 0) 
+            {
+                _age = value;
+            } else {
+                throw new ArgumentException("Age must be higher than 0");
+            }
+        }
+    }
+
+    //FName
+    public string FName
+    {
+        get => _fName;
+        set
+        {
+            if (value.Length > 1 && value.Length < 11)
+            {
+                _fName = value;
+            } else {
+                throw new ArgumentException("Firstname must have between 2 and 10 characters");
+            }
+        }
+    }
+
+    //LName
+    public string LName
+    {
+        get => _lName;
+        set
+        {
+            if (value.Length > 2 && value.Length < 16)
+            {
+                _lName = value;
+            } else {
+                throw new ArgumentException("Lastname must have between 3 and 15 characters");
+            }
+        }
+    }
+
+    //Height
+    public int Height
+    {
+        get => _height;
+        set => _height = value;
+    }
+
+    //Weight
+    public int Weight
+    {
+        get => _weight;
+        set => _weight = value;
+    }
+}
--- Ovning3/Program.cs
+++ Ovning3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//3.1) Inkapsling
+//Skapa en klass Person och ge den följande privata fält: age, fName, lName, height, weight
+
+var personHandler = new PersonHandler();
+Person person1;
+Person person2;
+Person person3;
+    try
+     {
+            person1 = personHandler.CreatePerson(20, "Anders", "Andersson", 181, 85);
+            person2 = personHandler.CreatePerson(30, "Zlatan", "Ibrahimovic", 198, 90);
+            person3 = personHandler.CreatePerson(40, "Greta", "Garbo", 159, 55);
+     }
+        catch (ArgumentException error)
+        {
+            Console.WriteLine(error.Message);
+        }
